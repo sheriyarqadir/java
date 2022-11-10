3e974ae529db4b5f0650d8bb391df13b0d17fb12
sheriyarqadir <116277563+sheriyarqadir@users.noreply.github.com>
2022-11-10 16:25:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+for (var i = 0; i <10; i++) {
+    console.log("3 x "+ (i+1) + " = " +3*(i+1));
+ }
+    var a = ["a","b","c","d","e"];
+    console.log=("g")
+    a.push("s")
+    console.log("a")
+    a.pop("d")
+    a.unshift("fish", "ferret");
+    a.splice(2, 2, "pig", "duck", "emu");
+    a.splice(2, 2);
+    
+    
+    var i = a.indexOf("b");
+    console.log(a[i]);
+    a.slice(1,3);
+    console.log(b);
+
+    push("b");
+    Pop();
+     slice();
+     unshift();
+
+
+   
